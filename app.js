29d275c9b597fac63994102ff767ce5259ac16e9
VictorFromTheInternet <vma0430@gmail.com>
2025-08-13 15:15:43 -0500

    hello world

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ import mongoose from 'mongoose'
 import path from 'path'
 import jsonwebtoken from 'jsonwebtoken'
 
-
+import healthCheck_router from './routers/helloWorld.js'
 import login_router from './routers/login.js'
 
 const app = express()
 
+const PORT = process.env.PORT
+
 app.use(express.json())
+app.use('/', healthCheck_router)
+app.use('/login', login_router)
 
 
-app.listen(()=>{
+app.listen(PORT,()=>{
     console.log(`Listening on port: ${PORT}`)
+    console.log(`http://localhost:${PORT}`)
 })
--- routers/helloWorld.js
+++ routers/helloWorld.js
@@ -1,10 +1,10 @@
 import express from 'express'
 const router = express.Router()
 
-
+// health check
 router.get('/', (req,res)=>{
 
-    res.send('Hello World')
+    res.send('<h1>Hello World</h1>')
 })
 
 export default router
