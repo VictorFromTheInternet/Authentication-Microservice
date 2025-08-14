84d3cdf3d6a4ad50788c78a0848a2ca8b4df2f24
VictorFromTheInternet <vma0430@gmail.com>
2025-08-14 12:59:15 -0500

    adding smtp service

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,11 @@ export const userSchema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},    
+    isAccountVerified: {type: Boolean, required: false, default: false},
+    verifyOtp: {type: String, required: false, default: ''},
+    verifyOtpExpiresAt: {type: Number, required: false, default: 0},    
+    resetOtp: {type: String, required: false, default: ''},
+    resetOtpExpiresAt: {type: String, required: false, default: 0},
 })
 
 
